550622d5332cbad544032b122b15d9b2f31479bc
Paul <paul.cordon@laposte.net>
2019-05-21 12:04:42 +0200

    Initial Commit

---
+++ Simpson Character Book/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simpson Character Book
+//
+//  Created by MAC-DIN-002 on 21/05/2019.
+//  Copyright © 2019 MAC-DIN-002. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
